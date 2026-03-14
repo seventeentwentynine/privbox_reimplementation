02930122ac33bfae8ad1b3dab7717de9d1d974d3
Shakthi Vel <vshakthi@hotmail.com>
2026-03-13 21:18:32 -0700

    Placeholder code

---
+++ src/core/storage.py
@@ -0,0 +1,6 @@
+class RuleStorage:
+    def __init__(self):
+        self.rules = []
+        self.rule_tuples = {}
+
+rule_storage = RuleStorage()
---
+++ src/core/tokenization.py
@@ -0,0 +1,6 @@
+class Tokenizer:
+    def tokenize(self, data: bytes):
+        # placeholder tokenizer for API wiring.
+        return data.split()
+
+tokenizer = Tokenizer()
