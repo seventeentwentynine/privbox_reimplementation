0aa4f939776ba3497bd068057a0b47454fadcbbe
Dean Alexander Santos Buño <94216668+deanalex-buno95@users.noreply.github.com>
2026-04-05 19:16:58 -0700

    Complete the function for plotting the `O(n)` of rule scaling.

--- eval/benchmark_plot.py
+++ eval/benchmark_plot.py
@@ -17,3 +17,21 @@ def plot_rule_scaling():
         print(f"[-] Could not find {csv_path}.")
         return
     
+    # Create DataFrame from CSV data.
+    df = pd.read_csv(csv_path)
+
+    # Set up the plot.
+    plt.figure(figsize=(8, 5))
+    plt.plot(df['n_rules'], df['preprocessing_time_sec'], marker='o', color='firebrick', label='Preprocessing Time')
+    plt.plot(df['n_rules'], df['traffic_inspection_time_sec'], marker='s', color='goldenrod', label='Traffic Inspection Time')
+
+    # Set up the grid.
+    plt.title('Middlebox Performance VS Number of Rules (O(n))')
+    plt.xlabel('Number of Rules (n)')
+    plt.ylabel('Time (seconds)')
+    plt.grid(True)
+    plt.legend()
+    plt.savefig("eval/results/rule_scaling_graph.png")
+    print("[*] Saved Rule Scaling Graph!")
+
+
