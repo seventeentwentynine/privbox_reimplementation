740305cdd27ba09e5718ef2642f8aadcd5c1660e
Dean Alexander Santos Buño <94216668+deanalex-buno95@users.noreply.github.com>
2026-04-05 18:54:18 -0700

    Start a new function for plotting the `O(n)` of rule scaling.

--- eval/benchmark_plot.py
+++ eval/benchmark_plot.py
@@ -8,3 +8,12 @@ import matplotlib.pyplot as plt
 import os
 
 
+def plot_rule_scaling():
+    """
+    Plot the O(n) of rule scaling.
+    """
+    csv_path = "eval/results/rule_scaling_metrics.csv"
+    if not os.path.exists(csv_path):
+        print(f"[-] Could not find {csv_path}.")
+        return
+    
